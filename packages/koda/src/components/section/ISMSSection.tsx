a489462d75fd8b4815c81534bf9a2f8f495c0ced
JeinJang <jeinjang99@gmail.com>
2025-06-19 10:58:12 +0900

    fix: isms 인증번호

--- packages/koda/src/components/section/ISMSSection.tsx
+++ packages/koda/src/components/section/ISMSSection.tsx
@@ -1,22 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import { withTranslation, WithTranslation } from "react-i18next";
-import MediaQuery from "react-responsive";
+
 import images from "src/images";
-import { media, desktopWidth, mobileWidth } from "src/utils/media";
-import {
-  H2,
-  H2Pre,
-  Body1Pre,
-  CaptionSpan,
-  CaptionPre,
-} from "src/components/Typography";
-import { translate } from "src/locales";
+import { media } from "src/utils/media";
+import { H2Pre, Body1Pre, CaptionPre } from "src/components/Typography";
 import { colors } from "src/styles/colors";
-import PrimaryButton from "src/components/button/PrimaryButton";
-import TypeformButton from "src/components/button/TypeformButton";
-import { typeformUri } from "src/utils/uri";
-import { CategoryType, ActionType, gTagEvent } from "src/configs/analytics";
 
 type Props = WithTranslation;
 
@@ -108,7 +97,7 @@ const ISMSSection = (props: Props) => {
           <Description>
             {`한국디지털에셋은 국내 가상자산 법규를 철저하게 준수하고 있습니다.
 2021년 9월 1일자로 정보보호 관리체계 인증(ISMS)을 획득하였으며,
-2021년 12월 16일 가상자산사업자(VASP) 신고를 완료하였습니다. (등록번호 2021-13)`}
+2021년 12월 16일 가상자산사업자(VASP) 신고를 완료하였습니다. (인증번호 ISMS-KISA-2021-140)`}
           </Description>
           <Caption>{`ISMS 인증범위: 가상자산 수탁 서비스(심사받지 않은 물리적 인프라 제외)
 유효기간: 2024-09-01 ~ 2027-08-31 `}</Caption>
