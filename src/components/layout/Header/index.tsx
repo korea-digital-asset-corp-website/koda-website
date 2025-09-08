d5892f0389caed4464597d3231ed3e9c2b2646c3
trophy198 <trophy198@users.noreply.github.com>
2025-09-08 17:23:45 +0900

    feat: Header 컴포넌트 고정 위치로 변경
    - Header를 sticky position 설정
    - layout css 높이 조정

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -5,8 +5,8 @@ import KodaMainLogo from './KodaMainLogo';
 const Header = () => {
   // todo aria label 등 시맨틱 태그 다국어 지원 필요
   return (
-    <header className="w-full border-b-[color:var(--color-gray-50)] border-b border-solid">
-      <div className="max-w-[1440px] pt-[13.5px] pb-[10.5px] mx-auto px-[40px]">
+    <header className="sticky bg-white top-0 w-full border-b-[color:var(--color-gray-50)] border-b border-solid">
+      <div className="max-w-[1440px] w-[1440px] pt-[13.5px] pb-[10.5px] mx-auto px-[40px]">
         <div className="flex items-center justify-between">
           <KodaMainLogo />
           <div className="flex items-center gap-14 text-label-lg font-semibold">
