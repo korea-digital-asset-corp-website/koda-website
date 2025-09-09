3eef5ce53694465c717e8a631bd90634402967be
trophy198 <trophy198@users.noreply.github.com>
2025-09-09 09:15:55 +0900

    chore: 헤더 컴포넌트 ActionButtons -> ContactButton 네이밍 변경

---
+++ src/components/layout/Header/ContactButton.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import { getTranslations } from 'next-intl/server';
+
+const ContactButton = async () => {
+  const t = await getTranslations('header.button');
+  return (
+    <div className="flex items-center gap-12">
+      <Link href="" className="gap-2.5 px-4 py-3 rounded bg-[var(--color-gray-900)] text-white">
+        {t('contact')}
+      </Link>
+    </div>
+  );
+};
+
+export default ContactButton;
