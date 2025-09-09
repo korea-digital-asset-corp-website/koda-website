1be95bbda91d2fc9c27eb98742471f41044e5f23
Trophy198 <100824183+Trophy198@users.noreply.github.com>
2025-09-09 15:20:51 +0900

    feat: Header 다국어 번역 토글 기능 구현 (#24)
    - global.css 내부에 전역 css 추가
    - 서비스 문의 버튼 hover 색상 변경
    - 다구겅 번역 토글 기능 추가

--- src/components/layout/Header/ContactButton.tsx
+++ src/components/layout/Header/ContactButton.tsx
@@ -5,7 +5,10 @@ const ContactButton = async () => {
   const t = await getTranslations('header.button');
   return (
     <div className="flex items-center gap-12">
-      <Link href="" className="gap-2.5 px-4 py-3 rounded bg-[var(--color-gray-900)] text-white">
+      <Link
+        href=""
+        className="gap-2.5 px-4 py-3 rounded bg-[var(--color-gray-900)] hover:bg-[var(--color-gray-700)] transition-colors text-white"
+      >
         {t('contact')}
       </Link>
     </div>
--- src/components/layout/Header/LanguageSwitcher.tsx
+++ src/components/layout/Header/LanguageSwitcher.tsx
@@ -1,16 +1,29 @@
+'use client';
+
 import Ic_Logo from '@/public/assets/icons/ic_global.svg';
+import { useLocale } from 'next-intl';
+import { usePathname, useRouter } from '@/i18n/navigation';
 
 const LanguageSwitcher = () => {
+  const locale = useLocale();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const toggleLanguage = () => {
+    const newLocale = locale === 'ko' ? 'en' : 'ko';
+    router.replace(pathname, { locale: newLocale });
+  };
+
+  const displayText = locale === 'ko' ? 'KR' : 'EN';
+
   return (
     <button
-      className="hidden lg:flex flex-row items-center gap-1"
+      className="hidden px-1.5 py-2 lg:flex flex-row items-center gap-1 rounded-[4px] hover:bg-[var(--color-gray-5050)] transition-colors"
       type="button"
-      aria-label="언어 선택"
-      aria-expanded="false"
-      aria-haspopup="true"
+      onClick={toggleLanguage}
     >
       <Ic_Logo />
-      KR
+      {displayText}
     </button>
   );
 };
