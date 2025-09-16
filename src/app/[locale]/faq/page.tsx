42da276ae3433aa679e9aecd795e5b782b3bca21
Trophy198 <100824183+Trophy198@users.noreply.github.com>
2025-09-16 14:17:17 +0900

    feat: Footer 벤처확인마크 추가 및 페이지 하단 레이아웃 조정 (#33)
    - footer 마진 공간 누락된곳 추가
    - footer 벤처확인마크 추가

--- src/app/[locale]/faq/page.tsx
+++ src/app/[locale]/faq/page.tsx
@@ -4,7 +4,7 @@ import FaqAccordion from '@/components/faq/FaqAccordion';
 const FaqPage = async () => {
   const t = await getTranslations('faq');
   return (
-    <div className="max-w-[1440px] w-full mt-32 px-5 lg:px-[200px] mx-auto">
+    <div className="max-w-[1440px] w-full mt-32 mb-20 lg:mb-[180px] px-5 lg:px-[200px] mx-auto">
       <h1 className="text-headline-sm lg:text-headline-lg mb-14 lg:mb-[120px] font-bold">{t('title')}</h1>
       <FaqAccordion />
     </div>
