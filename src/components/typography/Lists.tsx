3f6a1ce756f75a227781fd8ca7dcfe8b5b5ceb2d
trophy198 <trophy198@users.noreply.github.com>
2025-09-17 15:19:36 +0900

    feat: ol, ul, li 컴포넌트 확장

--- src/components/typography/Lists.tsx
+++ src/components/typography/Lists.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 interface ListProps {
   children: React.ReactNode;
   className?: string;
@@ -9,13 +11,29 @@ interface LiProps {
 }
 
 export function Ol({ children, className = '' }: ListProps) {
-  return <ol className={`text-body-sm lg:text-body-lg my-6 list-decimal list-inside pl-4 ${className}`}>{children}</ol>;
-}
-
-export function Ul({ children, className = '' }: ListProps) {
-  return <ul className={`text-body-sm lg:text-body-lg my-3 list-disc list-inside pl-4 ${className}`}>{children}</ul>;
+  return <ol className={`k-ol mt-3 mb-6 text-body-sm lg:text-body-lg ${className}`}>{children}</ol>;
 }
 
 export function Li({ children, className = '' }: LiProps) {
   return <li className={`text-body-sm lg:text-body-lg ${className}`}>{children}</li>;
+}
+
+export function SubOl({ children, className = '' }: ListProps) {
+  return <ol className={`k-subol ml-6 text-body-sm lg:text-body-lg ${className}`}>{children}</ol>;
+}
+
+export function SubLi({ children, className = '' }: LiProps) {
+  return <li className={`text-body-sm lg:text-body-lg ${className}`}>{children}</li>;
+}
+
+export function Ul({ children, className = '' }: ListProps) {
+  return <ul className={`my-3 pl-4 list-disc list-outside ${className}`}>{children}</ul>;
+}
+
+export function SubCircledOl({ children, className = '' }: ListProps) {
+  return <ol className={`k-circleol py-0 ml-6 text-body-sm lg:text-body-lg ${className}`}>{children}</ol>;
+}
+
+export function SubCircledLi({ children, className = '' }: LiProps) {
+  return <li className={`k-subcircled-li text-body-sm lg:text-body-lg ${className}`}>{children}</li>;
 }
