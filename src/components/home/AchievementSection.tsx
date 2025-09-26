40e5bfd08a73360c23b5f343b2c96c04f4ad378c
trophy198 <trophy198@users.noreply.github.com>
2025-09-26 11:11:26 +0900

    refactor: 메인페이지 벤처 기업 인증 섹션 height 변경
    - 텍스트 컴포넌트 기준 padding 상하 140px

--- src/components/home/AchievementSection.tsx
+++ src/components/home/AchievementSection.tsx
@@ -12,12 +12,12 @@ const AchievementSection = () => {
   return (
     <section
       ref={ref}
-      className={`relative mt-20 py-20 lg:py-32 bg-[var(--color-gray-5020)] transition-all duration-800 ease-out ${
+      className={`relative mt-20 pt-[230px] pb-[80px] lg:py-[140px] h-auto lg:min-h-[396px] bg-[var(--color-gray-5020)] transition-all duration-800 ease-out ${
         isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-20'
       }`}
     >
       <div className="max-w-[1440px] w-full px-10 mx-auto relative">
-        <div className="absolute -top-[105px] lg:-top-[157px] right-10 lg:right-20">
+        <div className="absolute -top-[255px] lg:-top-[170px] right-10 lg:right-20">
           <Image
             src={Venture_Enterprise_badge}
             alt={t('badge.alt')}
@@ -28,9 +28,9 @@ const AchievementSection = () => {
           />
         </div>
 
-        <div className="grid lg:grid-cols-2 gap-12 items-center relative z-10 pt-28">
-          <div className="space-y-3 lg:space-y-4 mt-32 max-lg:mt-40 lg:mt-0">
-            <h2 className="text-headline-sm lg:text-headline-lg font-bold lg:whitespace-pre-line">{t('title')}</h2>
+        <div className="grid lg:grid-cols-2 gap-12 items-center relative z-10">
+          <div className="space-y-3 lg:space-y-4 mt-[83px] lg:mt-0 whitespace-pre-line lg:whitespace-normal">
+            <h2 className="text-headline-sm lg:text-headline-lg font-bold">{t('title')}</h2>
             <p className="text-left text-body-md lg:text-body-lg lg:whitespace-pre-line">{t('description')}</p>
           </div>
         </div>
