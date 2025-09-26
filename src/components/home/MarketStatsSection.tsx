f17b0df99cadc9c6e435b89cccc7e0a9290b371a
trophy198 <trophy198@users.noreply.github.com>
2025-09-26 13:49:25 +0900

    메인페이지 MarketStats컴포넌트 description 폰트 스타일 body-xl 폰트 색상 gray700으로 변경

--- src/components/home/MarketStatsSection.tsx
+++ src/components/home/MarketStatsSection.tsx
@@ -27,7 +27,7 @@ const MarketStatsSection = () => {
           <div className="flex flex-col justify-between h-full lg:min-h-[570px] lg:flex-1 lg:pr-8">
             <div className="space-y-3 lg:space-y-4">
               <h2 className="text-headline-sm lg:text-headline-lg font-bold lg:whitespace-pre-line">{t('title')}</h2>
-              <p className="text-body-md lg:text-body-lg text-gray-600 lg:whitespace-pre-line">{t('description')}</p>
+              <p className="text-body-md lg:text-body-xl text-gray-700 lg:whitespace-pre-line">{t('description')}</p>
             </div>
 
             <div className="rounded-[4px] border border-gray-50 mt-8 lg:mt-0">
