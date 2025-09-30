f7de227ad443f0b087c9fbd2be2cb24aefeb78ac
Trophy198 <100824183+Trophy198@users.noreply.github.com>
2025-09-30 16:57:47 +0900

    refactor: 개인정보 처리 방침 및 공지사항 height 변경 (#68)

--- src/app/[locale]/faq/page.tsx
+++ src/app/[locale]/faq/page.tsx
@@ -4,7 +4,7 @@ import FaqAccordion from '@/components/faq/FaqAccordion';
 const FaqPage = async () => {
   const t = await getTranslations('faq');
   return (
-    <div className="max-w-[1440px] min-h-[486px] lg:min-h-auto w-full mt-[72px] lg:mt-32 mb-20 lg:mb-[148px] px-5 lg:px-[200px] mx-auto">
+    <div className="max-w-[1440px] w-full mt-[72px] lg:mt-32 mb-20 lg:mb-[180px] px-5 lg:px-[200px] mx-auto min-h-[486px] lg:min-h-[788px]">
       <h1 className="text-headline-sm lg:text-headline-lg mb-10 lg:mb-[88px] font-bold">{t('title')}</h1>
       <FaqAccordion />
     </div>
--- src/app/[locale]/notice/[id]/page.tsx
+++ src/app/[locale]/notice/[id]/page.tsx
@@ -27,7 +27,7 @@ const NoticeDetailPage = async ({ params }: NoticeDetailPageProps) => {
   }
 
   return (
-    <div className="max-w-[1440px] w-full mt-[72px] mb-20 lg:mt-32 px-5 lg:px-[200px] mx-auto">
+    <div className="max-w-[1440px] w-full mt-[72px] lg:mt-32 mb-20 lg:mb-[180px] px-5 lg:px-[200px] mx-auto min-h-[486px] lg:min-h-[788px]">
       <header className="pb-4 lg:pb-6 border-b border-[color:var(--color-gray-50)]">
         <h1 className="text-headline-sm lg:text-headline-lg font-bold text-gray-900 mb-2 lg:mb-3">
           {t(notice.titleKey)}
